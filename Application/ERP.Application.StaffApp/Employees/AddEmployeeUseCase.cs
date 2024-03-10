c10c8f233592bf6ed69eea3d8688be2008cfcd34
MadhaviHindagoda <kumari_hmms_e22@engug.ruh.ac.lk>
2024-03-11 00:27:42 +0530

    commit1

---
+++ Application/ERP.Application.StaffApp/Employees/AddEmployeeUseCase.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text;
+using System.Threading.Tasks;
+using ERP.Application.StaffApp.Interfaces;
+using ERP.Domain.Core.Entity;
+using ERP.Application.StaffApp.Employees.Interfaces;
+
+namespace ERP.Application.StaffApp.Employees
+{
+    public class AddEmployeeUseCase : IAddEmployeeUseCase
+    {
+        private readonly IEmployeeRepository _studentRepository;
+        private IEmployeeRepository _employeeRepository;
+
+        public AddEmployeeUseCase(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        public async Task ExecuteAsync(Employee employee)
+        {
+            await _employeeRepository.AddEmployeeAsync(employee);
+        }
+    }
+}
---
+++ Application/ERP.Application.StaffApp/Employees/Interfaces/IAddEmployeeUseCase.cs
@@ -0,0 +1,14 @@
+﻿using ERP.Domain.Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Application.StaffApp.Employees.Interfaces
+{
+    public interface IAddEmployeeUseCase
+    {
+        Task ExecuteAsync(Employee employee);
+    }
+}
