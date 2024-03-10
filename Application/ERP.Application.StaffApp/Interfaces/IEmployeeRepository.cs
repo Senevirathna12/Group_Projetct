9adbb1294535c9674d19bac25d262d97dda2b226
MadhaviHindagoda <kumari_hmms_e22@engug.ruh.ac.lk>
2024-03-11 00:59:15 +0530

    employee

---
+++ Application/ERP.Application.StaffApp/Interfaces/IEmployeeRepository.cs
@@ -0,0 +1,14 @@
+﻿using ERP.Domain.Core.Entity;
+
+namespace ERP.Application.StaffApp.Interfaces
+{
+    public interface IEmployeeRepository
+    {
+        Task<IEnumerable<Employee>> GetAllEmployeessAsync(string name);
+        Task<bool> ExistAsync(Employee employee);
+        Task AddEmployeeAsync(Employee employee);
+        Task EditEmployeeAsync(Employee employee);
+        Task<Employee> GetStudentById(int EmployeeId);
+        
+    }
+}
---
+++ Infrastructure/ERP.Repository.PgSql/Configurations/EmployeeConfigurations.cs
@@ -0,0 +1,34 @@
+﻿using Bogus;
+using ERP.Domain.Core.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ERP.Repository.PgSql.Configurations
+{
+    internal class EmployeeConfigurations : IEntityTypeConfiguration<Employee>
+    {
+        public void Configure(EntityTypeBuilder<Employee> builder)
+        {
+
+            builder.HasData(
+
+               Enumerable.Range(1, 10).Select(
+                   index => new Faker<Employee>()
+                         .RuleFor(s => s.EmployeeId, index + 1000)
+                         .RuleFor(s => s.FirstName, f => f.Name.FirstName())
+                         .RuleFor(s => s.LastName, f => f.Name.LastName())
+                         .RuleFor(s => s.Email, (f, u) => f.Internet.Email(u.FirstName, u.LastName))
+
+
+               )
+            );
+
+
+        }
+    }
+}
