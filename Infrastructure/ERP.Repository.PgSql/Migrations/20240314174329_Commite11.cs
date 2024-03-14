e31357aa95ccbf1348cec3d13b1bd226f3337238
Senevirathna12 <anishantha702@gmail.com>
2024-03-15 01:03:32 +0530

    Design

---
+++ Infrastructure/ERP.Repository.PgSql/Migrations/20240314174329_Commite11.cs
@@ -0,0 +1,298 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ERP.Repository.PgSql.Migrations
+{
+    /// <inheritdoc />
+    public partial class Commite11 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1001,
+                columns: new[] { "Email", "FirstName", "LastName", "RegistrationNum" },
+                values: new object[] { "Jamil.Kunze@yahoo.com", "Jamil", "Kunze", "EG/2020/2282" });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1002,
+                columns: new[] { "Email", "FirstName", "LastName", "RegistrationNum" },
+                values: new object[] { "Melba_Wintheiser@gmail.com", "Melba", "Wintheiser", "EG/2023/8280" });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1003,
+                columns: new[] { "Email", "FirstName", "LastName", "RegistrationNum" },
+                values: new object[] { "Kolby.Larkin@hotmail.com", "Kolby", "Larkin", "EG/2022/3915" });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1004,
+                columns: new[] { "Email", "FirstName", "LastName", "RegistrationNum" },
+                values: new object[] { "Della_Rogahn@gmail.com", "Della", "Rogahn", "EG/2023/5646" });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1005,
+                columns: new[] { "Email", "FirstName", "LastName", "RegistrationNum" },
+                values: new object[] { "Thad.Boehm78@hotmail.com", "Thad", "Boehm", "EG/2020/1693" });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1006,
+                columns: new[] { "Email", "FirstName", "LastName", "RegistrationNum" },
+                values: new object[] { "Jake.Jacobson44@hotmail.com", "Jake", "Jacobson", "EG/2022/4705" });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1007,
+                columns: new[] { "Email", "FirstName", "LastName", "RegistrationNum" },
+                values: new object[] { "Nellie43@gmail.com", "Nellie", "Turner", "EG/2022/4229" });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1008,
+                columns: new[] { "Email", "FirstName", "LastName", "RegistrationNum" },
+                values: new object[] { "Gianni.Welch58@yahoo.com", "Gianni", "Welch", "EG/2022/8267" });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1009,
+                columns: new[] { "Email", "FirstName", "LastName", "RegistrationNum" },
+                values: new object[] { "Orin.Wintheiser89@gmail.com", "Orin", "Wintheiser", "EG/2022/6348" });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1010,
+                columns: new[] { "Email", "FirstName", "LastName", "RegistrationNum" },
+                values: new object[] { "Veronica.Kunze70@yahoo.com", "Veronica", "Kunze", "EG/2021/2708" });
+
+            migrationBuilder.UpdateData(
+                table: "Teachers",
+                keyColumn: "TeacherId",
+                keyValue: 1001,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "Jaquelin56@yahoo.com", "Jaquelin", "Conn" });
+
+            migrationBuilder.UpdateData(
+                table: "Teachers",
+                keyColumn: "TeacherId",
+                keyValue: 1002,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "Tressie.Rolfson58@yahoo.com", "Tressie", "Rolfson" });
+
+            migrationBuilder.UpdateData(
+                table: "Teachers",
+                keyColumn: "TeacherId",
+                keyValue: 1003,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "Maverick.Kassulke@gmail.com", "Maverick", "Kassulke" });
+
+            migrationBuilder.UpdateData(
+                table: "Teachers",
+                keyColumn: "TeacherId",
+                keyValue: 1004,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "Elise55@yahoo.com", "Elise", "Mills" });
+
+            migrationBuilder.UpdateData(
+                table: "Teachers",
+                keyColumn: "TeacherId",
+                keyValue: 1005,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "Elian_Flatley27@hotmail.com", "Elian", "Flatley" });
+
+            migrationBuilder.UpdateData(
+                table: "Teachers",
+                keyColumn: "TeacherId",
+                keyValue: 1006,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "Roderick3@yahoo.com", "Roderick", "Kuphal" });
+
+            migrationBuilder.UpdateData(
+                table: "Teachers",
+                keyColumn: "TeacherId",
+                keyValue: 1007,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "Ismael72@hotmail.com", "Ismael", "Dooley" });
+
+            migrationBuilder.UpdateData(
+                table: "Teachers",
+                keyColumn: "TeacherId",
+                keyValue: 1008,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "Kaia74@yahoo.com", "Kaia", "Oberbrunner" });
+
+            migrationBuilder.UpdateData(
+                table: "Teachers",
+                keyColumn: "TeacherId",
+                keyValue: 1009,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "Pat.Medhurst@yahoo.com", "Pat", "Medhurst" });
+
+            migrationBuilder.UpdateData(
+                table: "Teachers",
+                keyColumn: "TeacherId",
+                keyValue: 1010,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "Emmet_Huels@yahoo.com", "Emmet", "Huels" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1001,
+                columns: new[] { "Email", "FirstName", "LastName", "RegistrationNum" },
+                values: new object[] { "Amalia92@yahoo.com", "Amalia", "Pollich", "EG/2021/8767" });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1002,
+                columns: new[] { "Email", "FirstName", "LastName", "RegistrationNum" },
+                values: new object[] { "Minerva.Purdy@yahoo.com", "Minerva", "Purdy", "EG/2020/7799" });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1003,
+                columns: new[] { "Email", "FirstName", "LastName", "RegistrationNum" },
+                values: new object[] { "Haleigh.Altenwerth49@yahoo.com", "Haleigh", "Altenwerth", "EG/2023/7096" });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1004,
+                columns: new[] { "Email", "FirstName", "LastName", "RegistrationNum" },
+                values: new object[] { "Hanna_Pacocha78@gmail.com", "Hanna", "Pacocha", "EG/2022/3988" });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1005,
+                columns: new[] { "Email", "FirstName", "LastName", "RegistrationNum" },
+                values: new object[] { "Irma77@yahoo.com", "Irma", "Ledner", "EG/2022/1842" });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1006,
+                columns: new[] { "Email", "FirstName", "LastName", "RegistrationNum" },
+                values: new object[] { "Osborne.Kreiger@hotmail.com", "Osborne", "Kreiger", "EG/2020/9737" });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1007,
+                columns: new[] { "Email", "FirstName", "LastName", "RegistrationNum" },
+                values: new object[] { "Marcel.Koelpin29@gmail.com", "Marcel", "Koelpin", "EG/2023/2318" });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1008,
+                columns: new[] { "Email", "FirstName", "LastName", "RegistrationNum" },
+                values: new object[] { "Ludwig4@hotmail.com", "Ludwig", "Langworth", "EG/2023/3748" });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1009,
+                columns: new[] { "Email", "FirstName", "LastName", "RegistrationNum" },
+                values: new object[] { "Meta_Osinski88@yahoo.com", "Meta", "Osinski", "EG/2020/9284" });
+
+            migrationBuilder.UpdateData(
+                table: "Students",
+                keyColumn: "StudentId",
+                keyValue: 1010,
+                columns: new[] { "Email", "FirstName", "LastName", "RegistrationNum" },
+                values: new object[] { "Joshua17@hotmail.com", "Joshua", "Larson", "EG/2022/6882" });
+
+            migrationBuilder.UpdateData(
+                table: "Teachers",
+                keyColumn: "TeacherId",
+                keyValue: 1001,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "Marlen35@gmail.com", "Marlen", "Kshlerin" });
+
+            migrationBuilder.UpdateData(
+                table: "Teachers",
+                keyColumn: "TeacherId",
+                keyValue: 1002,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "Ian82@yahoo.com", "Ian", "Streich" });
+
+            migrationBuilder.UpdateData(
+                table: "Teachers",
+                keyColumn: "TeacherId",
+                keyValue: 1003,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "Cleta.Beahan96@gmail.com", "Cleta", "Beahan" });
+
+            migrationBuilder.UpdateData(
+                table: "Teachers",
+                keyColumn: "TeacherId",
+                keyValue: 1004,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "Dino76@gmail.com", "Dino", "McLaughlin" });
+
+            migrationBuilder.UpdateData(
+                table: "Teachers",
+                keyColumn: "TeacherId",
+                keyValue: 1005,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "Theresa30@yahoo.com", "Theresa", "Veum" });
+
+            migrationBuilder.UpdateData(
+                table: "Teachers",
+                keyColumn: "TeacherId",
+                keyValue: 1006,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "Aileen9@hotmail.com", "Aileen", "Borer" });
+
+            migrationBuilder.UpdateData(
+                table: "Teachers",
+                keyColumn: "TeacherId",
+                keyValue: 1007,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "Ruben.Hessel91@hotmail.com", "Ruben", "Hessel" });
+
+            migrationBuilder.UpdateData(
+                table: "Teachers",
+                keyColumn: "TeacherId",
+                keyValue: 1008,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "Darron97@yahoo.com", "Darron", "Simonis" });
+
+            migrationBuilder.UpdateData(
+                table: "Teachers",
+                keyColumn: "TeacherId",
+                keyValue: 1009,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "Cassie52@gmail.com", "Cassie", "Bradtke" });
+
+            migrationBuilder.UpdateData(
+                table: "Teachers",
+                keyColumn: "TeacherId",
+                keyValue: 1010,
+                columns: new[] { "Email", "FirstName", "LastName" },
+                values: new object[] { "Lavinia.Jast71@hotmail.com", "Lavinia", "Jast" });
+        }
+    }
+}
